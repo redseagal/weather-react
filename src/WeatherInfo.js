791e7359d0a66d5cf121cda417442ac511b17e57
redseagal <70918566+redseagal@users.noreply.github.com>
2020-12-25 23:39:32 -0600

    Updated Weather Icon
    Added WeatherIcon componet, rendering the correct weather icon.

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import DateTime from "./DateTime";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -16,11 +17,9 @@ export default function WeatherInfo(props) {
       <div className="row">
         <div className="col-6">
           <div className="clearfix weather-temperature">
-            <img
-              src={props.data.imgUrl}
-              alt={props.data.description}
-              className="float-left"
-            />
+            <div className="float-left">
+              <WeatherIcon code={props.data.icon} />
+            </div>
             <div className="float-left">
               <strong>{Math.round(props.data.temperature)}</strong>
               <span className="units"></span>
